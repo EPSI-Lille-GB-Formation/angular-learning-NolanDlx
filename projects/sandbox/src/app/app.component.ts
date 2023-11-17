1cc62781af6613dfb5b1949343a2d4ec938a7567
NolanDlx <nolan.dlc19@gmail.com>
2023-11-17 13:50:17 +0100

    Premier commit

---
+++ projects/sandbox/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Hello word! SandBox</h1>
+  `,
+  styles: []
+})
+
+export class AppComponent {}
